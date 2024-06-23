20fb1348bc3186362f230dd2a1db7e12448e75cf
WWLagroconsultingltd <nkwi.loh@meltwater.org>
2024-06-23 12:46:05 +0000

    built APMA website

---
+++ Androidfarmer's webpage/script.js
@@ -0,0 +1,4 @@
+document.getElementById("download-btn").addEventListener("click", function() {
+    // Redirect to Google Play Store
+    window.location.href = "https://play.google.com/store/apps/details?id=com.wwlagroconsulting.apma"; 
+});
